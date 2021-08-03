7ef40fc027b55900c3b588c1cd86c1669d922328
csucoderlee <csucoderlee@foxmail.com>
2021-08-03 12:56:13 +0800

    test pointer

---
+++ src/ch15/pointer_test.go
@@ -0,0 +1,21 @@
+package ch15
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Dog struct {
+	name string
+}
+
+func (dog *Dog) SetName(name string) {
+	dog.name = name
+}
+
+func TestName(t *testing.T) {
+	dog := new(Dog)
+	dog.SetName("dogName")
+	fmt.Printf("the dog name is %s\n", dog.name)
+	fmt.Printf("the dog type is %T\n", dog)
+}
